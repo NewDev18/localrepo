91cca27008cb59d7dbaaa3563afa9b32b1d63f8e
NewDev18 <lamichhanerabin3@gmail.com>
2023-09-22 23:30:16 +0545

    push in main branch from qa branch

--- practice.py
+++ practice.py
@@ -1,2 +1,2 @@
-<b>this one is first row</b>
-<b> this one is second row of qa branch</b>
+<b>I have done changes in qa branch, Lets see in main branch</b>
+<b> this one is second row of qa branch</b> 
