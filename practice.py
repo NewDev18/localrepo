f707f52df22a969e7d2238e19daa9526d1ba3f19
NewDev18 <lamichhanerabin3@gmail.com>
2023-09-22 23:41:49 +0545

    qa branch

--- practice.py
+++ practice.py
@@ -1,2 +1,3 @@
 <b>I have done changes in qa branch, Lets see in main branch</b>
-<b> this one is second row of qa branch</b> 
+<b> this one is second row of qa aadbranch</b> 
+<b>this one is qa</b>
